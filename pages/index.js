a3f77cb1ff533eaf9feab904cdc788bf46a0792c
7d3v <107454750+7d3v@users.noreply.github.com>
2023-10-15 09:27:25 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,8 @@ export default function Home() {
       {/* left side */}
       <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
         <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome to Your Next.js App{" "}
-          <span className="block text-blue-500 text-2xl font-normal">
-            Deployed to DigitalOcean
-          </span>
+          65140035 - Thongchai Woraphong          
         </h2>
-
-        <p className="text-gray-700 mb-6">
-          Deploy API routes, static frontend, databases, and more.
-        </p>
-
-        <div className="sm:flex">
-          <a
-            href="https://www.digitalocean.com/docs/app-platform"
-            className="block py-2 px-5 rounded shadow bg-gray-500 text-gray-100 sm:mr-2 mb-2 sm:mb-0"
-          >
-            View the Docs
-          </a>
-          <a
-            href="https://cloud.digitalocean.com/apps"
-            className="block py-2 px-5 rounded shadow bg-blue-500 text-blue-100"
-          >
-            View Your Dashboard
-          </a>
-        </div>
       </div>
 
       {/* right side */}
